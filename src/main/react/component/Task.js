283d52b41cc962d69fdc04bedf8a8853bba27a40
silentsoft <hyesung.lee@silentsoft.org>
2021-10-19 11:03:05 +0900

    Add task delete feature

--- src/main/react/component/Task.js
+++ src/main/react/component/Task.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircle} from "@fortawesome/free-regular-svg-icons";
 
 import "./Task.css";
+import axios from "axios";
 
 function Task(props) {
-    console.log(props.id);
-    return <div className="task-container">
-        <div className="task-circle-checkbox">
+    const [hide, setHide] = useState(false);
+
+    function deleteTask() {
+        axios.delete(process.env.REACT_APP_URI + `/task/${props.id}`).then(response => {
+            setHide(true);
+        });
+    }
+
+    return hide ? null : <div className="task-container">
+        <div className="task-circle-checkbox" onClick={deleteTask}>
             <FontAwesomeIcon icon={faCircle}/>
         </div>
         {props.title}
